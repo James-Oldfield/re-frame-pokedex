21fd26cae74cb9f7b5ebee442079bf2e7862a0bf
James Oldfield <hi@james-oldfield.co>
2017-03-07 16:19:25 +0000

    add simple input that updates db on key change

--- src/cljs/pokedex/views.cljs
+++ src/cljs/pokedex/views.cljs
@@ -1,7 +1,22 @@
 (ns pokedex.views
-    (:require [re-frame.core :as re-frame]))
+ (:require [re-frame.core :refer [subscribe dispatch]]))
 
-(defn main-panel []
-  (let [name (re-frame/subscribe [:name])]
+;; Represents the search input DOM node.
+;; Dispatches the :update-search-term event to update the store's search-term value.
+(defn search-input
+  []
+  (let [search-term (subscribe [:search-term])]
     (fn []
-      [:div "Hello from " @name])))
+      (pr @search-term)
+      [:input {:id "search"
+               :value @search-term
+               :placeholder "enter pokemon here..."
+               :on-change #(dispatch [:update-search-term (-> % .-target .-value)])}])))
+
+(defn main-panel
+  []
+  (let [name (subscribe [:name])]
+    (fn []
+      [:div "Hello from " @name
+       [:hr]
+       [search-input]])))
