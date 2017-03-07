585b5c41028ca51a8fb2b8f43c5c0f8e1c014e17
James Oldfield <hi@james-oldfield.co>
2017-03-07 16:45:24 +0000

    store pokemon as a set

--- src/cljs/pokedex/db.cljs
+++ src/cljs/pokedex/db.cljs
@@ -3,6 +3,6 @@
 (def default-db
   {:name "re-frame"
    :search-term ""
-   :pokemon ["charmander"
-             "squirtle"
-             "bulbasaur"]})
+   :pokemon #{"charmander"
+              "squirtle"
+              "bulbasaur"}})
