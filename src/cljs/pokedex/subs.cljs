b91da582ebd7eeef8a745d76c9a1bfb595b89b81
James Oldfield <hi@james-oldfield.co>
2017-03-07 16:45:54 +0000

    add sub function to filter pokemon by the search term

--- src/cljs/pokedex/subs.cljs
+++ src/cljs/pokedex/subs.cljs
@@ -1,6 +1,7 @@
 (ns pokedex.subs
     (:require-macros [reagent.ratom :refer [reaction]])
-    (:require [re-frame.core :as re-frame]))
+    (:require [re-frame.core :as re-frame]
+              [clojure.string :as s]))
 
 (re-frame/reg-sub
  :name
@@ -11,3 +12,17 @@
   :search-term
   (fn [db]
    (:search-term db)))
+
+(re-frame/reg-sub
+  :pokemon
+  (fn [db]
+    (:pokemon db)))
+
+;; Filter the pokemon set by search-term
+(re-frame/reg-sub
+  :matching-pokemon
+  :<- [:pokemon]
+  :<- [:search-term]
+  (fn [[pokemon search-term] _]
+    (filter
+      #(s/includes? % search-term) pokemon)))
