a7ed9e4dc3293ee32d42572d5d920f4aa1d240ee
James Oldfield <hi@james-oldfield.co>
2017-03-08 10:46:10 +0000

    refactor http request to use declarative -fx

--- src/cljs/pokedex/events.cljs
+++ src/cljs/pokedex/events.cljs
@@ -1,12 +1,13 @@
 (ns pokedex.events
-    (:require [re-frame.core :as re-frame]
-              [ajax.core :refer [GET]]
-              [pokedex.db :as db]))
+  (:require [re-frame.core :as re-frame]
+            [ajax.core :as ajax]
+            [day8.re-frame.http-fx]  
+            [pokedex.db :as db]))
 
 (re-frame/reg-event-db
- :initialize-db
- (fn  [_ _]
-   db/default-db))
+  :initialize-db
+  (fn  [_ _]
+    db/default-db))
 
 ;; Update the existing search value a new value
 (re-frame/reg-event-db
@@ -14,12 +15,31 @@
   (fn [db [_ new-search-term]]
     (assoc db :search-term new-search-term)))
 
-;; GET the pokedex data
-(re-frame/reg-event-db
+(re-frame/reg-event-fx
   :get-pokemon
-  (fn [db _]
-    (GET
-      "http://pokeapi.co/api/v2/pokedex/2/"
-      {:handler #(dispatch [:process-response %1])
-       :error-handler #(dispatch [:bad-response %1])})
-    (assoc db :pokemon-loading? true)))
+  (fn [{db :db} _]
+    {:db (assoc db :pokemon-loading? true)
+     :http-xhrio {:method :get
+                  :uri "http://pokeapi.co/api/v2/pokedex/2/"
+                  :timeout 8000
+                  :response-format (ajax/json-response-format {:keywords? true})
+                  :on-success [:handle-http-success]
+                  :on-failure [:handle-http-error]}}))
+
+;; Handle http success
+(re-frame/reg-event-db
+  :handle-http-success
+  (fn [db [_ response]]
+    (-> db
+        (assoc :pokemon-loading? false
+               :pokemon-loading-failed? false
+               :pokemon (:pokemon_entries response)))))
+
+;; Handle http errors
+(re-frame/reg-event-db
+  :handle-http-error
+  (fn [db [_ response]]
+    (pr "fail" response)
+    (-> db
+        (assoc :pokemon-loading? false
+               :pokemon-loading-failed? true))))
